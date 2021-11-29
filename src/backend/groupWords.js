1c2cc9f327a585d947b488bec2a89e2f5e9d9bb6
eduardo <ev4lente@gmail.com>
2021-11-29 18:28:42 -0300

    Grouped words

---
+++ src/backend/groupWords.js
@@ -0,0 +1,37 @@
+module.exports = words => {
+    return new Promise((resolver, reject) => {
+        try {
+            // the words are duplicated after the reduce
+            const groupedWords = words.reduce((obj, word) => {
+               if(obj[word]) {
+                    obj[word] = obj[word] + 1
+               } else {
+                    obj[word] = 1
+               }
+               return obj
+            }, {})
+
+            const groupedWordsArray = Object
+                .keys(groupedWords)
+                // without parenthesis it is only a function body
+                .map(key => ({ name: key, amount: groupedWords[key] }))
+                .sort((w1, w2) => w2.amount - w1.amount)
+                
+            
+            resolver(groupedWordsArray)    
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+function filterValidRow(row) {
+    const notNumber = !parseInt(row.trim())
+    const notEmpty = !!row.trim()
+    const notInterval = !row.includes('-->')
+    return notNumber && notEmpty && notInterval
+}
+
+const removePunctuation = row => row.replace(/[,?!.-]/g, '')
+const removeTags = row => row.replace(/(<[^>]+)>/ig, '').trim()
+const mergeRows = (fullText, row) => `${fullText} ${row}`
